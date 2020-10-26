089e847944f4e7de96bbab3bcef44daea430ddf6
Mayank Agrawal <amayank@umich.edu>
2020-10-26 15:03:36 -0500

    now interactive can show connections on selected atom indices

--- PAvisualize/visualize_mayavi.py
+++ PAvisualize/visualize_mayavi.py
@@ -11,20 +11,44 @@ from mayavi.core.api import PipelineBase
 from traitsui.api import View, Item, Group
 from mayavi.core.ui.api import MayaviScene, SceneEditor, MlabSceneModel
 
+from . import quaternion
 
 
 
-def unwrap_pos(positions, Lx, Ly, Lz, buffer):
+
+def unwrap_points(points, ref_points, Lx, Ly, Lz):
+    """Accepts two arrays of points in 3D and box dimensions.
+    Arrays may have any shape as long as the last axis has 3D coordinates.
+    Unwraps the <point> position in case the distance between the <point> 
+    and <ref_point> is more than half the box length along the corresponding dimension.
+    Box dimensions are (0 to Lx) (0 to Ly) (0 to Lz).
+    """
+
+    shape = points.shape
+
+    points = points.reshape(-1,3)
+    ref_points = ref_points.reshape(-1,3)
     
-    #TODO
+    for i in range(len(points)):
+        if points[i,0]-ref_points[i,0] > Lx/2:
+            points[i,0] -= Lx
+        elif points[i,0]-ref_points[i,0] < -Lx/2:
+            points[i,0] += Lx
 
-    # for i in range(len(positions)):
-    #     if positions[i,0] - minx >= Lx/2:
-    #         pos[i,0] -= Lx
-    #     if pos[i,1] - miny >= Ly/2:
-    #         pos[i,1] -= Ly
+        if points[i,1]-ref_points[i,1] > Ly/2:
+            points[i,1] -= Ly
+        elif points[i,1]-ref_points[i,1] < -Ly/2:
+            points[i,1] += Ly
 
-    return positions
+
+        if points[i,2]-ref_points[i,2] > Lz/2:
+            points[i,2] -= Lz
+        elif points[i,2]-ref_points[i,2] < -Lz/2:
+            points[i,2] += Lz
+
+
+    return points.reshape(shape)
+
 
 
 
@@ -193,42 +217,29 @@ def mlab_drawaxes(color_, box):
 
 
 
-def draw_points_and_lines(points, lines, color):
+def draw_points_and_lines(points, lines, color, filename):
     ''' 
     points: N X M X 3
     lines: N X K X 2  (defines connections between points)
     '''
 
-    bonds_permol =[]
-    num_atoms    =[]
-    nmol         =[]
-    positions    =[]
+    
 
     
-    itpname = os.path.basename(itpfile).strip('.itp')
 
-    bonds_permol = get_bonds_per_molecule(itpfile)
-    num_atoms    = get_num_atoms(itpfile)
-    nmol         = get_num_molecules(topfile, itpname)
-    positions    = get_positions(grofile, trrfile, (0,num_atoms*nmol))
+    
+
+    # bonds_permol = get_bonds_per_molecule(itpfile)
+    # num_atoms    = get_num_atoms(itpfile)
+    # nmol         = get_num_molecules(topfile, itpname)
+    positions    = points
    
     
     num_frames = positions.shape[0]  
     
-    
 
-    if centralize:
-        # Doesnt work correctly for now
-        pos_ = np.copy(positions.reshape(num_frames, nmol, num_atoms, 3))
-        pos_[:,:] = pos_[:,:,0].reshape(num_frames,nmol,1,3)
-        pos_ = pos_.reshape(num_frames,-1,3)
+    sim_path = os.path.dirname(filename)+'/'
 
-        positions = PAanalysis.unwrap_points(positions, pos_, Lx,Ly,Lz)
-        
-        positions -= np.mean(positions, axis=1).reshape(-1,1,3)
-        # positions += [Lx/2, Ly/2, Lz/2]
-
-    
 
     mol_color = [84,39,143,0.6] #[117,107,177, 0.6]
     light_green = [161,215,106]
@@ -244,7 +255,7 @@ def draw_points_and_lines(points, lines, color):
 
     colors = [yellow, blue, red, light_green]
 
-    
+        
     def dump_frame(frame,index):
 
         mlab.options.offscreen = True
@@ -309,11 +320,12 @@ def draw_points_and_lines(points, lines, color):
         dump_frame(frame,i)
            
 
-    filename = sim_path+'movie.mp4'
+        
     os.system(f'ffmpeg -framerate 10 -i {sim_path}/%04d.png -s:v 1080x1080 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p \
         {filename}')
     
     os.system('rm %s'%sim_path+'0*.png')
+
 
 
 
@@ -684,16 +696,17 @@ def run_animation(box):
 
 
 
-
-def interactive_scene(itpfile, topfile, grofile, trrfile, bonds_alkyl, box, draw_box=True):
-    # itpnames is the list of names of different molecules that     
-
+def interactive_scene(itpfile, topfile, grofile, trrfile, bonds_alkyl, 
+    box, draw_box=True, mol_indices=None, atom_indices=None, centralize=False, unbreak_molecules=False):
+    ''' itpnames is the list of names of different molecules that
+    mol_indices is a list of molecules indices to be drawn. None will draw all molecules
+    '''
     bonds_permol =[]
     num_atoms    =[]
     nmol         =[]
     positions    =[]
 
-    
+        
     itpname = os.path.basename(itpfile).strip('.itp')
 
     bonds_permol = get_bonds_per_molecule(itpfile)
@@ -701,13 +714,43 @@ def interactive_scene(itpfile, topfile, grofile, trrfile, bonds_alkyl, box, draw
     nmol         = get_num_molecules(topfile, itpname)
     positions    = get_positions(grofile, trrfile, (0,num_atoms*nmol))
     
-    
+
     Lx = box['Lx']
     Ly = box['Ly']
     Lz = box['Lz']    
     
     num_frames = positions.shape[0]  
     
+
+    if mol_indices != None:
+        positions = positions.reshape(-1,nmol,num_atoms,3)[:,mol_indices]
+        nmol = len(mol_indices)
+        positions = positions.reshape(-1,nmol*num_atoms,3)
+
+
+    if atom_indices != None:
+        bonds_permol_=[]
+        for b in bonds_permol:
+            if (b[0] in atom_indices) and (b[1] in atom_indices):
+                bonds_permol_ += [b]
+        bonds_permol = bonds_permol_
+
+    if unbreak_molecules:
+        # draw the whole together even if it means to put it outside the box
+        positions = positions.reshape(-1,nmol,num_atoms,3)
+        for frame,frame_positions in enumerate(positions):
+            for mol_index, pos in enumerate(frame_positions):
+                positions[frame,mol_index] = unwrap_points(pos, np.array([pos[0]]*len(pos)), Lx, Ly, Lz)
+        positions = positions.reshape(-1,nmol*num_atoms,3)
+
+
+    if centralize:
+        positions -= np.mean(positions, axis=1, keepdims=True)
+
+
+    
+
+
 
     mol_color = [84,39,143,0.6] #[117,107,177, 0.6]
     light_green = [161,215,106]
@@ -839,7 +882,7 @@ def interactive_scene(itpfile, topfile, grofile, trrfile, bonds_alkyl, box, draw
 
 
 
-def interactive_scene_co(itpfiles, topfile, grofile, trrfile):
+def interactive_scene_co(itpfiles, topfile, grofile, trrfile, box):
     # itpnames is the list of names of different molecules that     
 
     bonds_permol =[]
@@ -856,7 +899,9 @@ def interactive_scene_co(itpfiles, topfile, grofile, trrfile):
         positions    += [get_positions(grofile, trrfile, (start,start+num_atoms[i]*nmol[i]))]
         start = num_atoms[i]*nmol[i]
     
-    
+    Lx = box['Lx']
+    Ly = box['Ly']
+    Lz = box['Lz']    
     
     num_frames = positions[0].shape[0]  
         
@@ -918,8 +963,8 @@ def interactive_scene_co(itpfiles, topfile, grofile, trrfile):
         
 
         box_color = [0,0,0,0.6]#[204,204,204, 0.7]
-        mlab_box(box_color)
-        mlab_drawaxes(box_color)
+        mlab_box(box_color, box)
+        mlab_drawaxes(box_color, box)
         mlab.view(azimuth=35, elevation=35, roll=0, distance=40, focalpoint=None)
         fig.scene.parallel_projection = True # orthogonal projection
         
@@ -994,6 +1039,234 @@ def interactive_scene_co(itpfiles, topfile, grofile, trrfile):
 
 
 
+
+
+def average_molecular_structure_scene(itpfile, topfile, grofile, trrfile, bonds_alkyl, frame_iterator, box):
+    ''' This is for single molecule type
+    itpnames is the list of names of different molecules that
+    mol_indices is a list of molecules indices to be drawn. None will draw all molecules
+    '''
+    
+
+    itpname = os.path.basename(itpfile).strip('.itp')
+
+    bonds_permol = get_bonds_per_molecule(itpfile)
+    num_atoms    = get_num_atoms(itpfile)
+    nmol         = get_num_molecules(topfile, itpname)
+    positions    = get_positions(grofile, trrfile, (0,num_atoms*nmol))
+    
+    positions = positions.reshape(-1,nmol,num_atoms,3)
+    
+    positions = positions[list(frame_iterator),:]
+    
+
+    Lx = box['Lx']
+    Ly = box['Ly']
+    Lz = box['Lz']    
+    
+    num_frames = positions.shape[0]
+    
+    
+
+    # atom indices from itpfile
+    pep_indices = []
+    PAM_indices = []
+    res_names = []
+    start = False
+    with open(itpfile, 'r') as f:
+        for line in f:
+            if '[ atoms ]' in line:
+                start = True
+                continue
+            if start:
+                words = line.split()
+                if words == []:
+                    break
+                if words[3] == 'PAM':
+                    PAM_indices += [int(words[0])-1]
+                else:
+                    pep_indices += [int(words[0])-1]
+                    res_names += [words[3]+'\n'+words[4]]
+
+    # reverse indices of PAM
+    atom_indices = PAM_indices[::-1] + pep_indices
+    res_names = ['PAM']*len(PAM_indices) + res_names
+
+    
+    # find binarynodes, branchednodes and leaves
+    
+    # make a dictionary of bonds
+    # 
+
+
+    # Calculate average structure by calculating average bond angle at each residue 
+    # and average dihedral angles
+    qs = []
+    dihedrals = []
+    for frame,frame_positions in enumerate(positions):
+        for mol_index, mol_positions in enumerate(frame_positions):
+            
+            # unbreak molecule
+            positions[frame,mol_index] = unwrap_points(mol_positions, np.array([mol_positions[0]]*len(mol_positions)), Lx, Ly, Lz)
+            
+
+            qs_ = []
+
+
+            # # shift first atom to origin
+            # positions[frame,mol_index] -= mol_positions[0]
+
+            # # rotate first relative position vector to z axis
+            # v1 = mol_positions[0] - mol_positions[1]
+            # v2 = [0,0,1]
+            # q = quaternion.q_between_vectors(v1, v2)
+            
+            # for atom_index, atom_position in enumerate(mol_positions):
+            #     positions[frame,mol_index,atom_index] = quaternion.qv_mult(q, atom_position)
+            
+            # # rotate first second relative position vector in xy plane to face x axis
+            # v1 = np.array([mol_positions[1,0],mol_positions[1,1],0]) - np.array([mol_positions[2,0],mol_positions[2,1],0])
+            # v2 = [1,0,0]
+            # q = quaternion.q_between_vectors(v1, v2)
+
+            # for atom_index, atom_position in enumerate(mol_positions):
+            #     positions[frame,mol_index,atom_index] = quaternion.qv_mult(q, atom_position)
+    
+
+
+
+    positions = np.mean(positions, axis=1)
+    positions = np.mean(positions, axis=0, keepdims=True)
+    nmol = 1
+    num_frames = 1
+
+    
+
+    mol_color = [84,39,143,0.6] #[117,107,177, 0.6]
+    light_green = [161,215,106]
+    yellow = [255, 217, 0]#[255,255,191]
+    yellow2 = [255,255,204]
+    purple = [118,42,131]
+    pink = [197,27,125]
+    orange = [255,127,0]
+    blue = [146,197,222]
+    red = [202,0,32]
+    mol_color = [84,39,143,0.6]
+    alkyl_color = purple+[0.6]
+    pep_color = light_green+[0.6]
+
+    colors = [yellow, blue, red, light_green]
+
+    
+
+    def draw_frame(frame, fig):
+
+        connections = []
+        connections_alkyl = []
+        connections_pep = []
+        for i in range(nmol):
+            for bond in bonds_permol:
+                X1 = positions[ frame, bond[0] + num_atoms*i ]
+                X2 = positions[ frame, bond[1] + num_atoms*i ]
+                if not np.linalg.norm(X1-X2) > 0.5*min( [Lx,Ly,Lz] ):
+                    connections = connections + [bond+num_atoms*i]
+                    if set(bond) in bonds_alkyl:
+                        connections_alkyl += [bond+num_atoms*i]
+                    else:
+                        connections_pep   += [bond+num_atoms*i]
+
+        
+        X = positions[frame]
+        connections = np.array(connections)
+        connections_alkyl = np.array(connections_alkyl)
+        connections_pep = np.array(connections_pep)
+
+
+        x=X[:,0]; y=X[:,1]; z=X[:,2]
+        src_alkyl = mlab.pipeline.scalar_scatter(x, y, z)
+        src_alkyl.mlab_source.dataset.lines = connections_alkyl
+        src_alkyl.update()
+        surf_alkyl = mlab.pipeline.surface(src_alkyl, color=tuple(np.array(alkyl_color[:3])/255),
+                opacity=alkyl_color[3], line_width=1., figure=fig)
+
+        src_pep = mlab.pipeline.scalar_scatter(x, y, z)
+        src_pep.mlab_source.dataset.lines = connections_pep
+        src_pep.update()
+        surf_pep = mlab.pipeline.surface(src_pep, color=tuple(np.array(pep_color[:3])/255),
+                opacity=pep_color[3], line_width=2, figure=fig)
+
+
+        
+        
+        # box_color = [0,0,0,0.6]#[204,204,204, 0.7]
+        # mlab_box(box_color, box)
+        # mlab_drawaxes(box_color, box)
+    
+        mlab.view(azimuth=35, elevation=35, roll=0, distance=40, focalpoint=None)
+        fig.scene.parallel_projection = True # orthogonal projection
+        
+        return surf_alkyl, surf_pep, src_alkyl, src_pep
+
+
+
+    def update_frame(frame,surf_alkyl,surf_pep,src_alkyl,src_pep):
+        
+        connections = []
+        connections_alkyl = []
+        connections_pep = []
+        for i in range(nmol):
+            for bond in bonds_permol:
+                X1 = positions[ frame, bond[0] + num_atoms*i ]
+                X2 = positions[ frame, bond[1] + num_atoms*i ]
+                if not np.linalg.norm(X1-X2) > 0.5*min( [Lx,Ly,Lz] ):
+                    connections = connections + [bond+num_atoms*i]
+                    if set(bond) in bonds_alkyl:
+                        connections_alkyl += [bond+num_atoms*i]
+                    else:
+                        connections_pep   += [bond+num_atoms*i]
+
+        
+        X = positions[frame]
+        connections = np.array(connections)
+        connections_alkyl = np.array(connections_alkyl)
+        connections_pep = np.array(connections_pep)
+
+        x=X[:,0]; y=X[:,1]; z=X[:,2]
+        surf_alkyl.mlab_source.reset(x=x,y=y,z=z)
+        src_alkyl.mlab_source.dataset.lines = connections_alkyl
+        
+        surf_pep.mlab_source.reset(x=x,y=y,z=z)
+        src_pep.mlab_source.dataset.lines = connections_pep
+
+
+
+    class MyModel(HasTraits):
+        # slider = Range(-10, num_frames, .1)
+        # slider = Range(0, num_frames-1, 0, mode='slider')
+        scene  = Instance(MlabSceneModel, (), width=10)
+
+        def __init__(self):
+            HasTraits.__init__(self)
+            self.args = draw_frame(0,fig=self.scene.mayavi_scene)
+
+
+        # @on_trait_change('slider')
+        # def slider_changed(self):
+        #     update_frame(self.slider, *self.args)
+        
+
+        view = View(Item('scene'),
+                    # Group("slider"), 
+                    width=400,
+                    resizable=True)
+
+                    
+    
+    mlab.figure(bgcolor=(1, 1, 1), size=(400, 400))
+    mlab.gcf().scene.parallel_projection = True # orthogonal projection
+    
+    my_model = MyModel()
+    my_model.configure_traits()
 
 
 
